99a0b257babe9cb25d540607eb141cf50b470eee
Tuấn Anh <138606608+HoangTuanAnh03@users.noreply.github.com>
2024-10-09 10:49:35 +0700

    initial commit

---
+++ src/test/java/com/tuananh/edu_v2/EduV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuananh.edu_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
